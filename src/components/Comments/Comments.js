b85f94b85c136d98afc1ff96a6761defd2d34b6a
Luno-front-end <63469409+Luno-front-end@users.noreply.github.com>
2021-02-17 22:47:34 +0200

    Update Comments.js

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -2,19 +2,19 @@ import s from "./Coments.module.css";
 
 export default function Comments({ userId, visibleComments }) {
   return (
-    visibleComments && (
-      <div className={s.containerVisibleComments}>
-        <h2 className={s.headerComents}>Comments</h2>
-        {visibleComments(userId).map(({ nameValue, commentValue }, index) => (
-          <div key={index} className={s.containerInfo}>
-            <h3>Name: {nameValue}</h3>
-            <p className={s.text}>
-              <span className={s.headerText}>Comment: </span>
-              {commentValue}
-            </p>
-          </div>
-        ))}
-      </div>
-    )
+    <div className={s.containerVisibleComments}>
+      <h2 className={s.headerComents}>Comments</h2>
+      {visibleComments(userId).map(({ nameValue, commentValue }, index) => (
+        <div key={index} className={s.containerInfo}>
+          <h3 className={s.name}>
+                  <span className={s.headerName }>Name:</span> {nameValue}
+          </h3>
+          <p className={s.text}>
+            <span className={s.headerText}>Comment: </span>
+            {commentValue}
+          </p>
+        </div>
+      ))}
+    </div>
   );
 }
